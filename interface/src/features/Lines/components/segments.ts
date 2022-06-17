0f6f2f847140e9e3e05aae544816c8e7bd42c362
Tcumulus <maarten.laureyssen@gmail.com>
2022-06-17 19:17:15 +0200

    FIX: change line algorithm to fully diagonal type

--- interface/src/features/Lines/components/segments.ts
+++ interface/src/features/Lines/components/segments.ts
@@ -7,30 +7,20 @@ interface Segment {
   distance: number
 }
 
-const segments: Segment[] = []
-
 export const addSegment = (x1: number, y1: number, x2: number, y2: number): Segment => {
-  const [dx, dy] = [x2 - x1, y2 - y1]
-  const [dx_abs, dy_abs] = [Math.abs(dx), Math.abs(dy)]
-  const [x_sign, y_sign] = [dx > 0 ? 1 : -1, dy > 0 ? 1 : -1]
-
-  let point = [x1, y1]
   const points: number[][] = []
-  for (let x_ = 0, y_ = 0; x_ < dx_abs || y_ < dy_abs;) {
-    const decision = (1 + 2*x_) * dy_abs - (1 + 2*y_) * dx_abs
-    if (decision === 0) { // diagonal
-      point = [point[0] + x_sign, point[1] + y_sign]
-      x_++; y_++
-    } else if (decision < 0) { // horizontal
-      point = [point[0] + x_sign, point[1]]
-      x_++
-    } else { // vertical
-      point = [point[0], point[1] + y_sign]
-      y_++
-    }
-    points.push(point)
-  }
+  const [dx, dy] = [x2 - x1, y2 - y1]
   const distance = Math.sqrt(dx**2 + dy**2)
+
+  //calculate points between two points
+  for (let i = 0; i <= distance; i++) {
+    let t = distance === 0 ? 0.0 : i / distance
+    const x = Math.round(lerp(x1, x2, t))
+    const y = Math.round(lerp(y1, y2, t))
+    points.push([x, y])
+  }
+  points.shift() //remove first element (city itself)
+  
   const segment = {
     x1: x1,
     y1: y1,
@@ -39,6 +29,9 @@ export const addSegment = (x1: number, y1: number, x2: number, y2: number): Segm
     points: points,
     distance: distance
   }
-  segments.push(segment)
-  return segments[segments.length - 1]
+  return segment
+}
+
+const lerp = (start: number, end: number, t: number): number => {
+  return start + t * (end-start);
 }
